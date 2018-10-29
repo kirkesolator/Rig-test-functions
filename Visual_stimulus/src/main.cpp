c15a8bb950955ee4da00c8ec23f6c093585afaa7
stensolator <tor.stensola@neuro.fchampalimaud.org>
2018-10-29 11:02:24 +0000

    Initialize

--- Visual_stimulus/src/main.cpp
+++ Visual_stimulus/src/main.cpp
@@ -1,7 +1,25 @@
 #include <Arduino.h>
 
+// LED pins on board that controls the 9px TV. Make sure it's correct!
+const int pinsLED[9] = {1,2,3,4,5,6,7,8,9};
+
+// Initiate counter
+int i;
+
 void setup() {
-  // put your setup code here, to run once:
+  // Open serial port
+  Serial.begin(9600);
+  
+  // Get pin n
+  int nPins = sizeof(pinsLED)/sizeof(pinsLED[0]);
+  
+  // Set pinmodes to output
+  for (i = 0; i <= nPins; i++)
+  {
+    pinMode(pinsLED[i],OUTPUT);
+  }
+  // Wait with next stage until serial monitor is running
+  while (! Serial);
 }
 
 void loop() {
