584615b885df06524dd1d9abe1e910c21c3e3417
stensolator <tor.stensola@neuro.fchampalimaud.org>
2018-11-13 11:34:31 +0000

    Lick detector function snippet

--- Lick_detector/src/main.cpp
+++ Lick_detector/src/main.cpp
@@ -1,9 +1,22 @@
 #include <Arduino.h>
 
+const int bRate = 9600; // Baud rate for serial com
+const int pinLD = 13; // Input pin
+bool LDstate = false; // Lick state set to 0 first (TODO: should it?)
+
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(bRate); // Open serial com
+  pinMode(pinLD,INPUT); // Set pinmode to input
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  if (digitalRead(pinLD) != LDstate){ // Compare detector state
+    LDstate = !LDstate;
+    if (LDstate){
+      Serial.print("Lick ON"); // Report lick on
+    }
+    else{
+      Serial.print("Lick OFF"); // Report lick off
+    }
+  }
 }
